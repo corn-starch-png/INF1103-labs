2161491c45485e92c1c2d0e77cf625f82b6ec727
David <2605574@sit.singaporetech.edu.sg>
2026-09-10 15:18:43 +0800

    requriement 2

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -1,1 +1,8 @@
 inventory = 0
+user_input = ""
+
+while user_input != "quit":
+    user_input = input("Enter a stock quantity to add to inventory (or type 'quit' to exit): ")
+    if user_input == "quit":
+        break
+print(f"Final inventory: {inventory}")
